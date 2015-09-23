8a7944fc4278f53dd75f3a52eca9fcf0505c9fe2
Nimish Jha <nimish.jha@gmail.com>
2015-09-24 00:27:51 +1000

    minor updates

--- nimbus.user.js
+++ nimbus.user.js
@@ -67,6 +67,24 @@ function listProperties(o)
 	return s;
 }
 
+// https://gist.github.com/minhnc/2333095
+function unRegisterAllEventListeners(obj)
+{
+	if ( typeof obj._eventListeners == 'undefined' || obj._eventListeners.length == 0 )
+	{
+		return;
+	}
+
+	for(var i = 0, len = obj._eventListeners.length; i < len; i++)
+	{
+		var e = obj._eventListeners[i];
+		xlog(e);
+		obj.removeEventListener(e.event, e.callback);
+	}
+
+	obj._eventListeners = [];
+}
+
 function hasClass(ele,cls)
 {
 	return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
