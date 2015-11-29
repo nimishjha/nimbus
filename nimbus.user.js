805a3c96f4dfe2a116dff36d0fbc18edb17bc76e
Nimish Jha <nimish.jha@gmail.com>
2015-11-29 17:09:33 +1100

    fixed copyright year

--- nimbus.user.js
+++ nimbus.user.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 
 //	Nimbus
-//	Copyright (C) 2011-2015 Nimish Jha 
+//	Copyright (C) 2008-2015 Nimish Jha 
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
