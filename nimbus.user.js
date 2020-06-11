23c469af94ca6d1fda8adb29709ad90e35c30637
Nimish Jha <nimish.jha@gmail.com>
2020-06-11 19:59:00 +1000

    wip

--- nimbus.user.js
+++ nimbus.user.js
@@ -4468,11 +4468,10 @@ function deleteElementsContainingText(selector, str)
 function deleteElementsWithClassOrIdContaining(str)
 {
 	const e = get("*");
-	let i = e.length;
-	while(i--)
+	for(let i = 0, ii = e.length; i < ii; i++)
 	{
 		const node = e[i];
-		if(~node.className.toString().indexOf(str) || ~node.id.toString().indexOf(str))
+		if(node && ~node.className.toString().indexOf(str) || ~node.id.toString().indexOf(str))
 			del(node);
 	}
 }
@@ -4480,11 +4479,11 @@ function deleteElementsWithClassOrIdContaining(str)
 function removeEmojis()
 {
 	const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-	const textnodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-	for(let i = 0, ii = textnodes.snapshotLength; i < ii; i++)
+	const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	for(let i = 0, ii = textNodes.snapshotLength; i < ii; i++)
 	{
-		const node = textnodes.snapshotItem(i);
-		node.data = node.data.replace(regex, "");
+		const textNode = textNodes.snapshotItem(i);
+		textNode.data = textNode.data.replace(regex, "");
 	}
 }
 
