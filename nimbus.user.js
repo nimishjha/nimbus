6ba75dd37e1d5d1ecc59b92facbe84ebdc9eca47
Nimish Jha <nimish.jha@gmail.com>
2020-06-12 16:34:31 +1000

    Code cleanup

--- nimbus.user.js
+++ nimbus.user.js
@@ -345,6 +345,11 @@ function getOrCreate(tagName, id, parent)
 	return newElem;
 }
 
+function getTextNodes()
+{
+	return document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+}
+
 function del(arg)
 {
 	if(!arg)
@@ -366,13 +371,13 @@ function debounce(func, delay)
 	{
 		var context = this;
 		var args = arguments;
-		var later = function()
+		var debounced = function()
 		{
 			timeout = null;
 			func.apply(context, args);
 		};
 		clearTimeout(timeout);
-		timeout = setTimeout(later, delay);
+		timeout = setTimeout(debounced, delay);
 	};
 }
 
@@ -535,12 +540,8 @@ function containsAllOfTheStrings(s, arrStrings)
 	let i = arrStrings.length;
 	let found = 0;
 	while(i--)
-	{
 		if(~s.indexOf(arrStrings[i]))
-		{
 			found++;
-		}
-	}
 	if(found === arrStrings.length)
 		return true;
 	return false;
@@ -551,10 +552,8 @@ function startsWithAnyOfTheStrings(s, arrStrings)
 	if(!s || typeof s !== "string")
 		return false;
 	for(let i = 0, ii = arrStrings.length; i < ii; i++)
-	{
 		if(s.indexOf(arrStrings[i]) === 0)
 			return true;
-	}
 	return false;
 }
 
@@ -4474,7 +4473,7 @@ function deleteElementsWithClassOrIdContaining(str)
 function removeEmojis()
 {
 	const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-	const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	const textNodes = getTextNodes();
 	for(let i = 0, ii = textNodes.snapshotLength; i < ii; i++)
 	{
 		const textNode = textNodes.snapshotItem(i);
@@ -4484,7 +4483,7 @@ function removeEmojis()
 
 function replaceNbsp()
 {
-	const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	const textNodes = getTextNodes();
 	for(let i = 0, ii = textNodes.snapshotLength; i < ii; i++)
 	{
 		const textNode = textNodes.snapshotItem(i);
