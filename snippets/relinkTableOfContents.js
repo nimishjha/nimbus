b06a7383c57aa98afb27806f17801115bc130871
Nimish Jha <nimish.jha@gmail.com>
2024-10-04 08:28:17 +1000

    relinkTableOfContents()

---
+++ snippets/relinkTableOfContents.js
@@ -0,0 +1,54 @@
+function assignIdsIfMissing(elems)
+{
+	for(let i = 0, ii = elems.length; i < ii; i++)
+	{
+		const elem = elems[i];
+		if(!elem.id)
+			elem.id = elem.tagName + "_" +  i;
+	}
+}
+
+function relinkTableOfContents(linksSelector, headingsSelector)
+{
+	function normalizeText(str)
+	{
+		return str.toLowerCase().trim().replace(/[^A-Za-z]/g, "");
+	}
+
+	const links = get(linksSelector);
+	const headings = get(headingsSelector);
+	if(!(links && headings))
+	{
+		showMessageBig("Failed to get one or both of links and headings");
+		return;
+	}
+
+	if(links[0].tagName !== "A")
+	{
+		console.log(`Expected <a>, found ${links[0].tagName}`);
+		return;
+	}
+
+	assignIdsIfMissing(headings);
+
+	const headingsByText = {};
+	for(const heading of headings)
+		headingsByText[normalizeText(heading.textContent)] = heading;
+
+	let numLinksFixed = 0;
+
+	for(const link of links)
+	{
+		const linkTextLower = normalizeText(link.textContent);
+		const heading = headingsByText[linkTextLower];
+		if(heading)
+		{
+			link.setAttribute("href", "#" + heading.id);
+			numLinksFixed++;
+		}
+	}
+
+	showMessageBig(`${numLinksFixed} links fixed`);
+}
+
+relinkTableOfContents(".markd a", "h1, h2, h3, h4, h5, h6");
