47734e4d9ab07daf2fa7ef1c8809ce574f31f890
Nimish Jha <nimish.jha@gmail.com>
2024-10-03 18:12:17 +1000

    moveIdToChild(), moveIdToParent()

---
+++ snippets/moveIdToParent.js
@@ -0,0 +1,34 @@
+function moveIdToParent(childSelector, parentSelector)
+{
+	const children = get(childSelector);
+	let numIdsMoved = 0;
+	let numNoId = 0;
+	let numNoParent = 0;
+	let numParentAlreadyHasId = 0;
+
+	for(const child of children)
+	{
+		if(!child.id)
+		{
+			numNoId++;
+			continue;
+		}
+		const parent = child.closest(parentSelector);
+		if(!parent)
+		{
+			numNoParent++;
+			continue;
+		}
+		if(parent.id)
+		{
+			numParentAlreadyHasId++;
+			continue;
+		}
+		parent.id = child.id;
+		child.removeAttribute("id");
+		numIdsMoved++;
+	}
+	showMessageBig(`Moved ${numIdsMoved} ids; ${numNoId} had no ID; ${numNoParent} had no matching parent; ${numParentAlreadyHasId}'s parent already had an ID`);
+}
+
+moveIdToParent(".markd", "h1, h2, h3, h4, h5, h6, p, li, blockquote, figure");
