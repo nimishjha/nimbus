b90ba79423fb17a65ea056aee9d1a6af1a3bf68b
Nimish Jha <nimish.jha@gmail.com>
2025-11-18 22:26:51 +1100

    fixTextAroundReferences: show count

--- snippets/fixTextAroundReferences.js
+++ snippets/fixTextAroundReferences.js
@@ -3,21 +3,29 @@ function fixTextAroundReferences(selector)
 	const regexPeriodOrClosingBracket = /^[\]\.]/;
 	const regexOpeningBracket = /\[$/;
 	const elems = get(selector);
+	let count = 0;
 	for(const elem of elems)
 	{
 		const next = elem.nextSibling;
 		if(next && next.nodeType === 3)
 		{
 			if(regexPeriodOrClosingBracket.test(next.data))
+			{
 				next.data = next.data.replace(regexPeriodOrClosingBracket, "");
+				count++;
+			}
 		}
 		const prev = elem.previousSibling;
 		if(prev && prev.nodeType === 3)
 		{
 			if(regexOpeningBracket.test(prev.data))
+			{
 				prev.data = prev.data.replace(regexOpeningBracket, "");
+				count++;
+			}
 		}
 	}
+	showMessageBig(`${count} text nodes fixed`);
 }
 
-fixTextAroundReferences(".markd");
+fixTextAroundReferences("footnote reference");
