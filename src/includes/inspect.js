23df2c2affc23fad419f18a1215922fb6d33ffbd
Nimish Jha <nimish.jha@gmail.com>
2025-12-04 00:10:44 +1100

    inspect: don't show element keys

--- src/includes/inspect.js
+++ src/includes/inspect.js
@@ -225,11 +225,11 @@ function handleMouseOver(evt)
 				}
 			}
 			inspectorPanel.appendChild(frag);
-			const keys = Object.keys(elem);
-			const elemKeys = document.createElement("em");
-			for(let i = 0, ii = keys.length; i < ii; i++)
-				elemKeys.appendChild(document.createTextNode(keys[i] + " "));
-			inspectorPanel.appendChild(elemKeys);
+			// const keys = Object.keys(elem);
+			// const elemKeys = document.createElement("em");
+			// for(let i = 0, ii = keys.length; i < ii; i++)
+			// 	elemKeys.appendChild(document.createTextNode(keys[i] + " "));
+			// inspectorPanel.appendChild(elemKeys);
 			const inspectorElem = document.getElementById("inspector");
 			inspectorElem.appendChild(inspectorPanel);
 		}
