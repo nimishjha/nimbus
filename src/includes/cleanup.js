807e4b67d50bfa3a6202624f760fd55d49945487
Nimish Jha <nimish.jha@gmail.com>
2025-12-20 14:07:27 +1100

    fixTextAroundReferences()

--- src/includes/cleanup.js
+++ src/includes/cleanup.js
@@ -844,3 +844,33 @@ export function replaceIncorrectHeading()
 		}
 	}
 }
+
+export function fixTextAroundReferences(selector = "footnote reference")
+{
+	const regexPeriodOrClosingBracket = /^[\]\.]/;
+	const regexOpeningBracket = /\[$/;
+	const elems = get(selector);
+	let count = 0;
+	for(const elem of elems)
+	{
+		const next = elem.nextSibling;
+		if(next && next.nodeType === 3)
+		{
+			if(regexPeriodOrClosingBracket.test(next.data))
+			{
+				next.data = next.data.replace(regexPeriodOrClosingBracket, "");
+				count++;
+			}
+		}
+		const prev = elem.previousSibling;
+		if(prev && prev.nodeType === 3)
+		{
+			if(regexOpeningBracket.test(prev.data))
+			{
+				prev.data = prev.data.replace(regexOpeningBracket, "");
+				count++;
+			}
+		}
+	}
+	showMessageBig(`${count} text nodes fixed`);
+}
