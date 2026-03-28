b15fa35e83a725c29be9fb382d100f74560cde69
Nimish Jha <nimish.jha@gmail.com>
2026-03-29 05:29:15 +1100

    getSafePrefixForSequentialIDs()

--- src/includes/interlinkReferences.js
+++ src/includes/interlinkReferences.js
@@ -1,6 +1,42 @@
 import { createUniqueID } from "./misc";
 import { logSuccess, logError } from "./log";
 
+export function getSafePrefixForSequentialIDs(prefix)
+{
+	prefix = prefix.toLowerCase();
+	const existingPrefixes = new Set();
+
+	const elems = document.querySelectorAll("body *[id]");
+	for(const elem of elems)
+	{
+		if(/^[a-z]+[0-9]/.test(elem.id))
+		{
+			const prefix = elem.id.match(/^[a-z]+/)[0];
+			existingPrefixes.add(prefix);
+		}
+	}
+
+	let charCode = 97;
+
+	let prefixToTest = prefix;
+	while(true)
+	{
+		if(existingPrefixes.has(prefixToTest))
+		{
+			prefixToTest = prefix + String.fromCharCode(charCode);
+			charCode++;
+			if(charCode > 122)
+			{
+				prefix += "_";
+				charCode = 97;
+			}
+		}
+		else
+			return prefixToTest;
+	}
+	return false;
+}
+
 export function interlink(primaryLink, secondaryLink, text, id)
 {
 	primaryLink.textContent = secondaryLink.textContent = text;
@@ -14,15 +50,23 @@ export function interlinkReferencesByIndex(footnoteRefs, nonFootnoteRefs)
 {
 	if(nonFootnoteRefs.length === footnoteRefs.length)
 	{
-		for(let i = 0, ii = nonFootnoteRefs.length; i < ii; i++)
+		const prefix = getSafePrefixForSequentialIDs("ref");
+		if(prefix)
 		{
-			const index = (i + 1).toString();
-			interlink(nonFootnoteRefs[i], footnoteRefs[i], index, "ref" + index);
+			for(let i = 0, ii = nonFootnoteRefs.length; i < ii; i++)
+			{
+				const index = (i + 1).toString();
+				interlink(nonFootnoteRefs[i], footnoteRefs[i], index, prefix + index);
+			}
+			showMessageBig(`${nonFootnoteRefs.length} reference pairs interlinked`);
 		}
-		logSuccess(`${nonFootnoteRefs.length} reference pairs interlinked`);
+		else
+		{
+			showMessageBig("Could not find a safe prefix; cannot interlink");
+		}
 	}
 	else
 	{
-		logError(`interlinkReferencesByIndex: ${nonFootnoteRefs.length} non-footnote refs, ${footnoteRefs.length} footnote refs; cannot interlink`);
+		showMessageError(`interlinkReferencesByIndex: ${nonFootnoteRefs.length} non-footnote refs, ${footnoteRefs.length} footnote refs; cannot interlink`);
 	}
 }
