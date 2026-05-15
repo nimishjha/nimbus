c0b54d5481aed53698ff02e9b2868f233fad04ab
Nimish Jha <nimish.jha@gmail.com>
2026-05-16 04:23:41 +1000

    replaceEmptyParagraphsWithHr: provide feedback

--- src/includes/fixBrokenHtml.js
+++ src/includes/fixBrokenHtml.js
@@ -6,7 +6,8 @@ import { removeRedundantHrs, replaceBrs } from "./cleanup";
 import { deleteEmptyTextNodes, deleteEmptyElements } from "./delete";
 import { convertElement } from "./replaceElements";
 import { isBlockElement, hasAdjacentBlockElement } from "./elementAndNodeTests";
-import { INLINE_TAGS} from "./constants";
+import { INLINE_TAGS } from "./constants";
+import { showMessageBig } from "./ui";
 
 //	If any text nodes or inline elements have a block element as a sibling, they need to be wrapped in a block container.
 export function rescueOrphanedInlineElements()
@@ -152,11 +153,16 @@ export function normaliseWhitespaceForParagraphs()
 export function replaceEmptyParagraphsWithHr()
 {
 	const elems = get("p");
+	let count = 0;
 	let i = elems.length;
 	while(i--)
 	{
 		if(getTextLength(elems[i]) === 0 || /^[_•\*—]+$/.test(elems[i].textContent.replace(/\s+/g, "")))
+		{
 			elems[i].replaceWith(document.createElement("hr"));
+			count++;
+		}
 	}
+	showMessageBig(`${count} hrs created`);
 	removeRedundantHrs();
 }
